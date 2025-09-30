c519698873068d00b2ee4b673b53273bdbb2e70e
Digs-LS <diego.lucas.sousa@live.com>
2025-09-30 15:16:39 -0300

    feat: project setup with spring initializr

---
+++ binha/src/main/java/br/gov/mdhc/ncia/binha/BinhaApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mdhc.ncia.binha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BinhaApplication.class, args);
+	}
+
+}
---
+++ binha/src/test/java/br/gov/mdhc/ncia/binha/BinhaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mdhc.ncia.binha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
